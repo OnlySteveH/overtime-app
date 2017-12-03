812232ab07540e0eaff2bfcd2c3859375bdd52c3
danale <danale@danales-MacBook-Pro.local>
2017-12-03 15:12:04 -0500

    Installed pundit for permission structure

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,16 +4,25 @@
 #
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
+
+# if in the future I want to have a super_user, editor_user, read-only_admin_user, etc.
+# do this to avoid hardcoding AdminUser, via this custom method of list of users.
 module Admin
   def self.admin_types
     ['AdminUser']
   end
 
   class ApplicationController < Administrate::ApplicationController
+    # to ensure user is signed_in
     before_action :authenticate_user!
     before_action :authenticate_admin
 
     def authenticate_admin
+      # authentication logic for admin
+      # unless the current_user is of try the type of the list of admin_types of AdminUser,
+      # give them this message and redirect to homepage,
+      # otherwise if the object of admin_types is of AdminUser is present allow user
+      # to proceed as admin.
       unless Admin.admin_types.include?(current_user.try(:type))
         flash[:alert] = "You are not authorized to access this page."
         redirect_to(root_path)
